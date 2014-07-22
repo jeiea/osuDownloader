b5068694dbec179a4e2df0f01dcd71f5c3934376
jeiea <solum5013@gmail.com>
2014-07-22 22:47:41 +0900

    후킹 코드 작성. 빌드 문제가 시급

---
+++ RemoteDown/OsuInjectee.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using EasyHook;
+using System.Runtime.InteropServices;
+
+namespace RemoteDown
+{
+
+public class OsuInjectee : EasyHook.IEntryPoint
+{
+	OsuDownloader.InvokeDownload Interface;
+	LocalHook CreateFileHook;
+	Queue<string> Queue = new Queue<string>();
+
+	public OsuInjectee(RemoteHooking.IContext context, string channelName)
+	{
+		// connect to host...
+		Interface = RemoteHooking.IpcConnectClient<OsuDownloader.InvokeDownload>(channelName);
+
+		Interface.Ping();
+	}
+
+	public void Run(RemoteHooking.IContext context, string channelName)
+	{
+		// install hook...
+		try
+		{
+			CreateFileHook = LocalHook.Create(
+								 LocalHook.GetProcAddress("shell32.dll", "ShellExecuteExW"),
+								 new DShellExecuteEx(ShellExecuteEx_Hooked),
+								 this);
+
+			CreateFileHook.ThreadACL.SetExclusiveACL(new int[] { 0 });
+		}
+		catch (Exception extInfo)
+		{
+			Interface.ReportException(extInfo);
+
+			return;
+		}
+
+		Interface.IsInstalled(RemoteHooking.GetCurrentProcessId());
+
+		RemoteHooking.WakeUpProcess();
+
+		// wait for host process termination...
+		try
+		{
+			while (true)
+			{
+				Thread.Sleep(500);
+
+				// transmit newly monitored file accesses...
+				if (Queue.Count > 0)
+				{
+					string[] package = null;
+
+					lock (Queue)
+					{
+						package = Queue.ToArray();
+
+						Queue.Clear();
+					}
+
+					foreach (var fileName in package)
+					{
+						Interface.OnBeatmapBrowse(RemoteHooking.GetCurrentProcessId(), fileName);
+					}
+				}
+				else
+					Interface.Ping();
+			}
+		}
+		catch
+		{
+			// Ping() will raise an exception if host is unreachable
+		}
+		finally
+		{
+			Interface.OnTerminate();
+		}
+	}
+
+	#region ShellExecuteEx pinvoke
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SHELLEXECUTEINFO
+	{
+		public int cbSize;
+		public uint fMask;
+		public IntPtr hwnd;
+		[MarshalAs(UnmanagedType.LPTStr)]
+		public string lpVerb;
+		[MarshalAs(UnmanagedType.LPTStr)]
+		public string lpFile;
+		[MarshalAs(UnmanagedType.LPTStr)]
+		public string lpParameters;
+		[MarshalAs(UnmanagedType.LPTStr)]
+		public string lpDirectory;
+		public int nShow;
+		public IntPtr hInstApp;
+		public IntPtr lpIDList;
+		[MarshalAs(UnmanagedType.LPTStr)]
+		public string lpClass;
+		public IntPtr hkeyClass;
+		public uint dwHotKey;
+		public IntPtr hIcon;
+		public IntPtr hProcess;
+	}
+
+	[DllImport("shell32.dll", CharSet = CharSet.Auto)]
+	static extern bool ShellExecuteEx(ref SHELLEXECUTEINFO lpExecInfo);
+
+	[UnmanagedFunctionPointer(CallingConvention.StdCall,
+							  CharSet = CharSet.Unicode,
+							  SetLastError = true)]
+	delegate bool DShellExecuteEx(ref SHELLEXECUTEINFO lpExecInfo);
+
+	static bool ShellExecuteEx_Hooked(ref SHELLEXECUTEINFO lpExecInfo)
+	{
+
+		try
+		{
+			OsuInjectee This = (OsuInjectee)HookRuntimeInfo.Callback;
+
+			if (lpExecInfo.lpFile.StartsWith("http://osu.ppy.sh/b/") ||
+				lpExecInfo.lpFile.StartsWith("http://osu.ppy.sh/s/"))
+			{
+				lock (This.Queue)
+				{
+					This.Queue.Enqueue(lpExecInfo.lpFile);
+				}
+			}
+
+			return true;
+		}
+		catch
+		{
+		}
+
+		//call original API...
+		return ShellExecuteEx(ref lpExecInfo);
+	}
+
+	#endregion
+}
+
+}
---
+++ osuDownloader/Hooker.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using EasyHook;
+using System.Windows;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+
+namespace OsuDownloader
+{
+
+public class InvokeDownload : MarshalByRefObject
+{
+	static string Cookie;
+	static string DownloadDir = "C:\\";
+
+	public void IsInstalled(int clientPid)
+	{
+		Console.WriteLine("FileMon has been installed in target {0}.\r\n", clientPid);
+	}
+
+	public void OnBeatmapBrowse(int clientPid, string fileName)
+	{
+		StringBuilder query = new StringBuilder("http://bloodcat.com/osu/?mod=json");
+
+		char idKind = fileName[18];
+		query.Append("&m=");
+		query.Append(idKind);
+
+		query.Append("&q=");
+		query.Append(fileName.Split('/')[4]);
+
+		WebClient client = new WebClient();
+		client.Encoding = Encoding.UTF8;
+
+		byte[] json = client.DownloadData(new Uri(query.ToString()));
+		JObject result = JObject.Parse(Encoding.UTF8.GetString(json));
+
+		int count = (int)result["resultCount"];
+		if (count != 1)
+		{
+			MessageBox.Show("해당하는 비트맵 수가 " + count + "개 입니다.");
+			return;
+		}
+
+		var beatmapJson = result["results"][0];
+		int beatmapId = (int)beatmapJson["id"];
+		string beatmapTitle = (string)beatmapJson["title"];
+		beatmapTitle = beatmapTitle.Except(System.IO.Path.GetInvalidFileNameChars()).ToString();
+		string downloadPath = DownloadDir + beatmapTitle + ".osz";
+		client.DownloadFile("http://bloodcat.com/osu/m/" + beatmapId, downloadPath);
+
+		ProcessStartInfo psi = new ProcessStartInfo();
+		psi.Verb = "open";
+		psi.FileName = downloadPath;
+		psi.UseShellExecute = true;
+		Process.Start(psi);
+	}
+
+	public void OnTerminate()
+	{
+		OsuHooker.IsHooked = false;
+	}
+
+	public void ReportException(Exception info)
+	{
+		Console.WriteLine("The target process has reported an error:\r\n" + info.ToString());
+	}
+
+	public void Ping()
+	{
+	}
+}
+
+class OsuHooker
+{
+	static string ChannelName = null;
+	public static bool IsHooked;
+
+	public static bool Hook()
+	{
+		if (IsHooked)
+			return true;
+
+		var osuCandidates = from proc in Process.GetProcesses()
+							where proc.ProcessName == "osu!"
+							select proc;
+
+		if (osuCandidates.Count() != 1)
+		{
+			return false;
+		}
+
+		int targetPid = osuCandidates.First().Id;
+
+		try
+		{
+			string thisFile = new Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+			string injectee = System.IO.Path.GetDirectoryName(thisFile) + "\\RemoteDown.dll";
+
+			try
+			{
+				Config.Register("Osu beatmap downloader.", thisFile, injectee);
+			}
+			catch (ApplicationException)
+			{
+
+				MessageBox.Show("DLL파일이 있는지, 관리자 권한이 있는지 확인해주세요.",
+								"후킹 실패", MessageBoxButton.OK);
+
+				System.Diagnostics.Process.GetCurrentProcess().Kill();
+			}
+
+			RemoteHooking.IpcCreateServer<InvokeDownload>(ref ChannelName, WellKnownObjectMode.SingleCall);
+
+			RemoteHooking.Inject(targetPid, injectee, injectee, ChannelName);
+		}
+		catch (Exception extInfo)
+		{
+			var log = System.IO.File.AppendText("osuDownloader.log");
+			log.WriteLine("------------------------");
+			var inner = extInfo;
+			while (inner != null)
+			{
+				log.WriteLine(extInfo.Message);
+				inner = inner.InnerException;
+			}
+			log.WriteLine("------------------------");
+			log.Close();
+			return false;
+		}
+
+		IsHooked = true;
+		return true;
+	}
+}
+}
