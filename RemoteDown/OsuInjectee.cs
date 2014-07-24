449228e2ae9de9299d4358f37f72e7b2f1dece78
jeiea <solum5013@gmail.com>
2014-07-25 02:27:40 +0900

    전체화면 시 화면 나가기 방지를 위한 ShowWindow 후킹 추가

--- RemoteDown/OsuInjectee.cs
+++ RemoteDown/OsuInjectee.cs
@@ -14,6 +14,9 @@ public class OsuInjectee : EasyHook.IEntryPoint
 {
 	OsuDownloader.InvokeDownload Interface;
 	LocalHook ShellExecuteExHook;
+	/// <summary>   ShowWindow function hook. This is necessary during fullscreen mode. </summary>
+	LocalHook ShowWindowHook;
+	bool IsHooked;
 	Queue<string> Queue = new Queue<string>();
 	ManualResetEvent QueueAppended;
 
@@ -32,10 +35,14 @@ public class OsuInjectee : EasyHook.IEntryPoint
 		{
 			ShellExecuteExHook = LocalHook.Create(
 									 LocalHook.GetProcAddress("shell32.dll", "ShellExecuteExW"),
-									 new DShellExecuteEx(ShellExecuteEx_Hooked),
-									 this);
+									 new DShellExecuteEx(ShellExecuteEx_Hooked), this);
+
+			ShowWindowHook = LocalHook.Create(
+								 LocalHook.GetProcAddress("user32.dll", "ShowWindow"),
+								 new DShowWindow(ShowWindow_Hooked), this);
 
 			ShellExecuteExHook.ThreadACL.SetExclusiveACL(new int[] { 0 });
+			ShowWindowHook.ThreadACL.SetExclusiveACL(new int[] { 0 });
 		}
 		catch (Exception extInfo)
 		{
@@ -85,7 +92,10 @@ public class OsuInjectee : EasyHook.IEntryPoint
 		finally
 		{
 			ShellExecuteExHook.Dispose();
+			ShowWindowHook.Dispose();
+
 			ShellExecuteExHook = null;
+			ShowWindowHook = null;
 		}
 	}
 
@@ -137,6 +147,10 @@ public class OsuInjectee : EasyHook.IEntryPoint
 					instance.Queue.Enqueue(lpExecInfo.lpFile);
 				}
 				instance.QueueAppended.Set();
+
+				// For prevention of window minimization in fullscreen mode.
+				instance.IsHooked = true;
+
 				return true;
 			}
 
@@ -150,6 +164,122 @@ public class OsuInjectee : EasyHook.IEntryPoint
 	}
 
 	#endregion
+
+	#region ShowWindow pinvoke
+
+	enum ShowWindowCommands
+	{
+		/// <summary>   Hides the window and activates another window. </summary>
+		Hide = 0,
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>
+		/// Activates and displays a window. If the window is minimized or maximized, the system restores
+		/// it to its original size and position. An application should specify this flag when displaying
+		/// the window for the first time.
+		/// </summary>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		Normal = 1,
+
+		/// <summary>   Activates the window and displays it as a minimized window. </summary>
+		ShowMinimized = 2,
+
+		/// <summary>   Maximizes the specified window. </summary>
+		Maximize = 3,
+
+		/// <summary>   Activates the window and displays it as a maximized window. </summary>
+		ShowMaximized = 3,
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>
+		/// Displays a window in its most recent size and position. This value is similar to
+		/// <see cref="Win32.ShowWindowCommand.Normal"/>, except the window is not activated.
+		/// </summary>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		ShowNoActivate = 4,
+
+		/// <summary>   Activates the window and displays it in its current size and position. </summary>
+		Show = 5,
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>
+		/// Minimizes the specified window and activates the next top-level window in the Z order.
+		/// </summary>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		Minimize = 6,
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>
+		/// Displays the window as a minimized window. This value is similar to
+		/// <see cref="Win32.ShowWindowCommand.ShowMinimized"/>, except the
+		/// window is not activated.
+		/// </summary>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		ShowMinNoActive = 7,
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>
+		/// Displays the window in its current size and position. This value is similar to
+		/// <see cref="Win32.ShowWindowCommand.Show"/>, except the window is not activated.
+		/// </summary>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		ShowNA = 8,
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>
+		/// Activates and displays the window. If the window is minimized or maximized, the system
+		/// restores it to its original size and position. An application should specify this flag when
+		/// restoring a minimized window.
+		/// </summary>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		Restore = 9,
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>
+		/// Sets the show state based on the SW_* value specified in the STARTUPINFO structure passed to
+		/// the CreateProcess function by the program that started the application.
+		/// </summary>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		ShowDefault = 10,
+
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		/// <summary>
+		/// <b>Windows 2000/XP:</b> Minimizes a window, even if the thread that owns the window is not
+		/// responding. This flag should only be used when minimizing windows from a different thread.
+		/// </summary>
+		////////////////////////////////////////////////////////////////////////////////////////////////////
+		ForceMinimize = 11
+	}
+
+	[DllImport("user32.dll")]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommands nCmdShow);
+
+	[UnmanagedFunctionPointer(CallingConvention.StdCall)]
+	delegate bool DShowWindow(IntPtr hWnd, ShowWindowCommands nCmdShow);
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	/// <summary>
+	/// Hook function which corresponds to ShowWIndow. It seems osu minimize own window after
+	/// ShellExecute during fullscreen mode. So it's necessary.
+	/// </summary>
+	///
+	/// <param name="hWnd">     Window handle. </param>
+	/// <param name="nCmdShow"> ShowWindowCommands. </param>
+	///
+	/// <returns>   Always true. </returns>
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	static bool ShowWindow_Hooked(IntPtr hWnd, ShowWindowCommands nCmdShow)
+	{
+		if (nCmdShow == ShowWindowCommands.Minimize)
+		{
+			return true;
+		}
+		return ShowWindow(hWnd, nCmdShow);
+	}
+
+	#endregion
+
 }
 
 }
