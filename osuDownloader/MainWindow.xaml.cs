5f5e2b18f471768a3cda2a6781fcc9b71b533ec9
jeiea <solum5013@gmail.com>
2014-07-30 10:39:05 +0900

    난독화 문제로 트레이 코드를 XAML에서 코드 비하인드로 변경. Obfuscar 프로젝트 임베딩한 어셈블리 제거.

--- osuDownloader/MainWindow.xaml.cs
+++ osuDownloader/MainWindow.xaml.cs
@@ -23,11 +23,43 @@ public partial class MainWindow : Window
 {
 	/// <summary>   The timer which check hooking is enabled. </summary>
 
+	Hardcodet.Wpf.TaskbarNotification.TaskbarIcon Tray;
+	MenuItem ToggleHookItem;
+
 	public MainWindow()
 	{
 		InitializeComponent();
 
 		MascotBtn.IsChecked = OsuHooker.IsInstalled;
+
+		#region Tray registration routine
+
+		// Xaml seems to have problem with project settings.
+		Tray = new TaskbarIcon()
+		{
+			IconSource = new BitmapImage(new Uri("pack://application:,,,/Pic/osuIcon.ico")),
+			ToolTipText = "Osu Beatmap Downloader v1.0",
+			DoubleClickCommand = NavigationCommands.FirstPage,
+		};
+
+		var contextMenu = new ContextMenu();
+		contextMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.MousePoint;
+		contextMenu.IsVisibleChanged += ContextMenu_IsVisibleChanged;
+
+		var headers = new[] { "창 띄우기", "켜기", "종료" };
+		var clicks = new RoutedEventHandler[] { MenuWindow_Click, ToggleHooking, MenuExit_Click };
+		for (int i = 0; i < headers.Length; i++)
+		{
+			var item = new MenuItem();
+			item.Header = headers[i];
+			item.Click += clicks[i];
+			contextMenu.Items.Add(item);
+		}
+
+		Tray.ContextMenu = contextMenu;
+		ToggleHookItem = (MenuItem)contextMenu.Items[1];
+
+		#endregion
 
 		OsuHooker.IsHookingChanged += CheckHooking;
 	}
